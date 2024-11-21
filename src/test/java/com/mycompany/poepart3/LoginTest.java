6731fe3ce609ddc82bb3f1d52cf86c3753d1819f
RC_Student_lab <RC_Student_lab@10.0.0.224>
2024-11-21 12:34:19 +0000

    ninthcommint

---
+++ src/test/java/com/mycompany/poepart3/LoginTest.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.mycompany.poepart3;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class LoginTest {
+    
+    public LoginTest() {
+    }
+   
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        Login.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+       
+    }
+
+    /**
+     * Test of loginUser method, of class Login.
+     */
+    @Test
+    public void testLoginUser() {
+        System.out.println("loginUser");
+        String username = "kyl_1";
+        String password = "Ch&&sec@ke99!";
+        boolean expResult = true;
+        boolean result = Login.loginUser(username, password);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+       
+    }
+
+    /**
+     * Test of returnLoginStatus method, of class Login.
+     */
+    @Test
+    public void testReturnLoginStatus() {
+        System.out.println("returnLoginStatus");
+        String username = "kyl_1";
+        String password = "Ch&&sec@ke99!";
+        String expResult = "Welcome " + username + ", it is great to see you again.";
+        String result = Login.returnLoginStatus(username, password);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+      
+    }
+    
+}
